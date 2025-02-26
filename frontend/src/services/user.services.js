5a460584cb8dfc17790ab0a5f0f92056d22aa511
Mayank Vishwakarma <mayankvishw09@gmail.com>
2025-02-26 23:12:41 +0530

    feat : frontend - user services

--- frontend/src/services/user.services.js
+++ frontend/src/services/user.services.js
@@ -9,3 +9,34 @@ export const userSignup = async (data) => {
     body: JSON.stringify(data),
   });
 };
+
+export const userLogin = async (data) => {
+  return await fetch(`${URL}/api/user/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+export const userDetails = async () => {
+  return await fetch(`${URL}/api/user/userdetails`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${localStorage.getItem("token")}`,
+    },
+  });
+};
+
+export const updateUser = async (data) => {
+  return await fetch(`${URL}/api/user/updateuser`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(data),
+  });
+};
