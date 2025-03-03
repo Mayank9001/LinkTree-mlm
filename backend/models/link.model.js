ce9ffaed6307c005400e48aa499f755f679ff6f8
Mayank Vishwakarma <mayankvishw09@gmail.com>
2025-03-03 11:23:34 +0530

    link schema

--- backend/models/link.model.js
+++ backend/models/link.model.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
 const linkSchema = new mongoose.Schema({
-  linkId: { 
-    type: String, 
-    required: true, 
-    unique: true 
-  },
   linkTitle: { type: String },
   linkUrl: { type: String },
   linkType: {
     type: String,
-    enum: ["applink", "shoplink"],
+    enum: ["app", "shop"],
   },
-  clicks: { 
-    type: Number, 
-    default: 0 
+  show: {
+    type: Boolean,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+  appType: {
+    type: String,
   },
   shopImg: {
-    data: Buffer,
-    contentType: String,
+    type: String,
   },
   profileId: {
     type: mongoose.Schema.Types.ObjectId,
